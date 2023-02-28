95e6c8968bc3d2a779012d50bd7cba80c280a876
CominatoMatteo <121224252+CominatoMatteo@users.noreply.github.com>
2023-02-28 09:16:16 +0100

    read exercice
    addedd read exercice

---
+++ Read/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Read
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char tasto;
+            bool trovato = false;
+            const int maxParole=3;
+            int contaparole=0;
+            string parola = "", numeri = "";
+            string[] dizionario = new string[maxParole];
+            Console.WriteLine("inserire sequenza di caratteri");
+            do
+            {
+                tasto = Convert.ToChar(Console.Read());
+                if (Char.IsLetter(tasto))
+                {
+                    parola = parola + Convert.ToChar(tasto);
+                }
+                else if (tasto == ' ' || tasto==13)
+                {
+                    if (contaparole == maxParole)
+                    {
+                        Console.WriteLine("dizionario pieno");
+                    }
+                    else
+                    {
+                        trovato = false;
+                        for (int i = 0; i < contaparole && !trovato; i++)
+                        {
+                            trovato = parola == dizionario[i];
+                        }
+                        if (!trovato)
+                        {
+                            dizionario[contaparole] = parola;
+                            contaparole++;
+                        }
+                        else
+                        {
+                            trovato = true;
+                        }
+                        parola = "";
+                    }
+                }
+            } while (tasto != 13);
+            Console.ReadLine(); // salta line feed
+            Console.ReadLine(); // serve per bloccare programma
+        }
+    }
+}
+//la mamma va al mercato 
+//la
+//mamma
+//va
+//al
+//mercato
